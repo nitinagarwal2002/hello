edb7f4e03053a04ba1fc5367e1bebf423478ad6c
nitinagarwal2002 <90981133+nitinagarwal2002@users.noreply.github.com>
2021-09-21 10:16:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Nitin Here!</h1>
+      <h2>I am very happy to use react.</h2>
+      <img
+        src="https://th.bing.com/th/id/OIP.V8EVWtmWPk9RwCPvy1wgtQHaEo?w=271&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7"
+        alt=""
+      />
+    </div>
+  );
+}
